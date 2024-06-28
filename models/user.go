caf3b7d21cc814691c6c1144010729afc792cb0a
MrPrograMax <grigorievma9461@gmail.com>
2024-06-29 06:15:58 +0700

    fix: micro fix

--- models/user.go
+++ models/user.go
@@ -3,7 +3,7 @@ package models
 // UserDto represents a user data transfer object
 // @Description Worker model
 type UserDto struct {
-	Id       int64  `json:"-"`
+	Id       int64  `json:"id"`
 	FullName string `json:"full_name" binding:"required" db:"full_name"`
 	RoleId   int64  `json:"role_id" binding:"required" db:"role_id"`
 }
